b9054fc142a4e76d34e8327e759208d90d08cc9d
RedbackThomson <redbackthomson@gmail.com>
2017-04-11 22:00:40 -0700

    Linted server

--- server.js
+++ server.js
@@ -11,14 +11,11 @@
   var logger = require('morgan');
   var bodyParser = require('body-parser');
   var methodOverride = require('method-override');
-  var static_dir = require('serve-static');
+  var staticDir = require('serve-static');
   var errorHandler = require('errorhandler');
   var helmet = require('helmet');
   var throng = require('throng');
   var cookieParser = require('cookie-parser');
-  var LocalStrategy = require('passport-local').Strategy;
-  var jwt = require('express-jwt');
-  var timeout = require('connect-timeout');
   var flash = require('connect-flash');
   var device = require('express-device');
   var mailer = require('nodemailer');
@@ -27,17 +24,22 @@
   require('dotenv').config({silent: process.env.NODE_ENV !== 'development'});
 
   var WORKERS = process.env.WEB_CONCURRENCY || 1;
+
   //Create workers on all the threads
-  throng({
-    workers: WORKERS,
-    lifetime: Infinity
-  }, start);
+  if (process.env.NODE_ENV === 'development') {
+    start();
+  } else {
+    throng({
+      workers: WORKERS,
+      lifetime: Infinity
+    }, start);
+  }
 
   // The application
   function start() {
     var app = express();
     var port = process.env.PORT || 3000;
-    var server = app.listen(port);
+    app.listen(port);
 
     require('./routes/redirects')(app);
     app.use(sslRedirect());
@@ -46,11 +48,6 @@
     // Extras
     // Rendering tools
     app.locals.moment = require('moment');
-
-    // Passport Logic
-    var passport = require('passport');
-    var User = require('./entities/users/model');
-    var Sponsor = require('./entities/sponsors/model');
 
     // Node mailer
     var transporter = mailer.createTransport({
@@ -67,32 +64,37 @@
     app.set('views', path.join(__dirname, 'views'));
     app.set('view engine', 'jade');
     app.use(favicon(path.join(__dirname, 'static/assets/img/favicon.png')));
-    if('development' == app.get('env'))
-      app.use(logger('dev'))
-    else
+    if (process.env.NODE_ENV === 'development') {
+      app.use(logger('dev'));
+    } else {
       app.use(logger('common', {
-        skip: function (req, res) { return res.statusCode < 400 }
+        skip: function (req, res) {
+          return res.statusCode < 400;
+        }
       }));
+    }
     app.use(cookieParser());
     app.use(bodyParser.json({type: 'application/vnd.api+json', limit: '50mb'}));
     app.use(device.capture());
-    app.use(bodyParser.urlencoded({extended: true, limit: '50mb', parameterLimit: 3000}));
+    app.use(bodyParser.urlencoded({extended: true, 
+      limit: '50mb', parameterLimit: 3000}));
 
     app.use(passport.initialize());
     app.use(flash());
     app.use(methodOverride('X-HTTP-Method-Override'));
-    app.use(static_dir(path.join(__dirname, 'static')));
-    var appRoutes = require('./routes/index')(app, process.env);
-    var apiRoutes = require('./routes/api')(app, process.env, transporter);
+    app.use(staticDir(path.join(__dirname, 'static')));
+
+    // Routing
+    require('./routes/index')(app, process.env);
+    require('./routes/api')(app, process.env, transporter);
     app.get('*', function(req, res){
       res.status(404);
       res.render('error', {'error': 'Page not found'});
     });
 
-
     // Error Handling
     // Development only
-    if ('development' == app.get('env')) {
+    if (process.env.NODE_ENV === 'development') {
       app.use(errorHandler());
     } else {
       // 404
@@ -101,13 +103,14 @@
       });
 
       // 500
-      app.use(function(err, req, res, next) {
+      app.use(function(err, req, res) {
        res.sendStatus(500);
       });
     }
 
     http.createServer(app).listen(app.get(port), function(){
-      console.log('Express server listening on port ' + port + ' on ' + WORKERS + ' worker(s)');
+      console.log('Express server listening on port ' + port + ' on ' +
+        WORKERS + ' worker(s)');
     });
   }
 }).call(this);
