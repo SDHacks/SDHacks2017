5cd216a47e1f0eeb698e346c53ed68f404c1bdd1
liawesomesaucer <vincent.oddrepublic@gmail.com>
2017-07-17 21:52:42 -0700

    Attempt fix form

--- static/assets/js/apply-info.js
+++ static/assets/js/apply-info.js
@@ -1,48 +1,48 @@
 $(document).ready(function() {
   //Load the application form
-  var applyForm = $(".apply-form");
+  var applyForm = $('.apply-form');
 
   function mustMatch(event, ui) {
     var source = $(this).val();
-    var temp = $(".ui-autocomplete li").map(function () { return $(this).text(); }).get();
+    var temp = $('.ui-autocomplete li').map(function () { return $(this).text(); }).get();
     var found = $.inArray(source, temp);
 
-    if(found < 0) {
+    if (found < 0) {
       var val = $(this).val().toLowerCase().trim();
-      if(val === 'ucsd' || val === 'uc san diego' || val === 'university of california san diego') {
+      if (val === 'ucsd' || val === 'uc san diego' || val === 'university of california san diego') {
         $(this).val('The University of California, San Diego');
       }
     }
   }
 
   //Load in the autocomplete values
-  $.getJSON("/assets/schools.json").done(function(data) {
-    $(".js-apply-form__universities").autocomplete({
+  $.getJSON('/assets/schools.json').done(function(data) {
+    $('.js-apply-form__universities').autocomplete({
       source: data,
       change: mustMatch
     });
     //CSS fix for Foundation
-    $('.ui-autocomplete').addClass('dropdown menu').attr("data-dropdown-menu", "");
+    $('.ui-autocomplete').addClass('dropdown menu').attr('data-dropdown-menu', '');
 
     //new Foundation.DropdownMenu($('.ui-autocomplete'), {});
   });
 
-  $.getJSON("/assets/majors.json").done(function(data) {
-    $(".js-apply-form__majors").autocomplete({
+  $.getJSON('/assets/majors.json').done(function(data) {
+    $('.js-apply-form__majors').autocomplete({
       source: data,
       change: mustMatch
     });
     //CSS fix for Foundation
-    $('.ui-autocomplete').addClass('dropdown menu').attr("data-dropdown-menu", "");
+    $('.ui-autocomplete').addClass('dropdown menu').attr('data-dropdown-menu', '');
 
     //new Foundation.DropdownMenu($('.ui-autocomplete'), {});
   });
 
-  $(".js-apply-form__open").click(function(e) {
+  $('.js-apply-form__open').click(function(e) {
     applyForm.slideDown(500, function() {
       //Ensure it only opens once
-      if(!applyForm.hasClass("js-apply-form__orbit")) {
-        $(".slick-container").slick({
+      if (!applyForm.hasClass('js-apply-form__orbit')) {
+        $('.slick-container').slick({
           infinite: false,
           draggable: false,
           arrows: false,
@@ -51,7 +51,7 @@ $(document).ready(function() {
           adaptiveHeight: true
         });
         $('#outcomeStmt').restrictLength( $('#outcomeStmt-length-element') );
-        applyForm.addClass("js-apply-form__orbit");
+        applyForm.addClass('js-apply-form__orbit');
 
         $.validate({
           modules : 'file',
@@ -61,11 +61,11 @@ $(document).ready(function() {
       $('html,body').animate({
         scrollTop: applyForm.offset().top
       }, 150, 'swing');
-      $("input:visible:first", applyForm).focus();
+      $('input:visible:first', applyForm).focus();
     });
   });
 
-  $(".js-apply-form__close").click(function(e) {
+  $('.js-apply-form__close').click(function(e) {
     applyForm.slideUp(500, function() {
       $('html,body').animate({
         scrollTop: 0
@@ -74,68 +74,69 @@ $(document).ready(function() {
   });
 
   var showError = function(message) {
-    $(".js-apply-form__error--message").text(message);
-    $(".js-apply-form__error").css('display', 'block');
+    $('.js-apply-form__error--message').text(message);
+    $('.js-apply-form__error').css('display', 'block');
   };
 
-  $(".apply-form").submit(function(e) {
+  $('.apply-form').submit(function(e) {
     return false;
   });
 
   // Disable the Major selection for High Schools
-  $("#institution-radio").on("change", function(e) {
-    if (this.value === "hs") {
-      $("#major").val("");
-      $("#major-error-message").text("");
-      $("#major").css("border-color", "#cacaca");
-      $("#major").prop("data-validation", "");
-      $("#major").prop("disabled", true);
+  $('#institution-radio').on('change', function(e) {
+    if (this.value === 'hs') {
+      $('#major').val('');
+      $('#major-error-message').text('');
+      $('#major').css('border-color', '#cacaca');
+      $('#major').prop('data-validation', '');
+      $('#major').prop('disabled', true);
     } else {
-      $("#major").prop("disabled", false);
-      $("#major").prop("data-validation", "required");
+      $('#major').prop('disabled', false);
+      $('#major').prop('data-validation', 'required');
     }
   });
 
-  $("#select-month,#select-date,#select-year").each(function() {
-    $(this).on("change", function(e) {
-      validateUniYear(e); 
+  $('#select-month,#select-date,#select-year').each(function() {
+    $(this).on('change', function(e) {
+      validateUniYear(e);
     });
   });
-  $("#institution-uni").on("input", function(e) {
+  $('#institution-uni').on('input', function(e) {
     validateUniYear(e);
   });
 
-  $("#institution-uni").focusout(function(e) {
+  $('#institution-uni').focusout(function(e) {
     validateUniYear(e);
   });
 
   function validateUniYear (e) {
     // Can be any age
-    if ($("#institution-uni").val().indexOf("The University of California") !== -1) {
+    if ($('#institution-uni').val().indexOf('The University of California') !== -1) {
       // All ages allowed
-      $("#select-year").attr("data-validation-allowing", "");
-      $("#select-month").attr("data-validation-allowing", "");
-      $("#age-error-message").text("");
-      $("#select-year").css("border-color", "#cacaca");
-    }
-    else {
+      $('#select-year').attr('data-validation-allowing', '');
+      $('#select-month').attr('data-validation-allowing', '');
+      $('#age-error-message').text('');
+      $('#select-year').css('border-color', '#cacaca');
+    }  else {
       // Needs to be over 18 - check age
-      $("#select-year").css("border-color", "rgb(185, 74, 72)");
-      $("#select-year").attr("data-validation-allowing", "range[1980;1998]");
-      if (parseInt($("#select-year").val()) === 1998)
-        $("#select-month").attr("data-validation-allowing", "range[1;9]");
-      else $("#select-month").attr("data-validation-allowing", "");
+      $('#select-year').css('border-color', 'rgb(185, 74, 72)');
+      $('#select-year').attr('data-validation-allowing', 'range[1980;1998]');
+      if (parseInt($('#select-year').val()) === 1998) {
+        $('#select-month').attr('data-validation-allowing', 'range[1;9]');
+      } else {
+        $('#select-month').attr('data-validation-allowing', '');
+      }
     }
   }
 
-  $(".js-apply-form__button--final").click(function(e) {
-    var form = $(".apply-form");
-    if (!$("#apply-form__slide-2").isValid()) {
+  $('.js-apply-form__button--final').click(function(e) {
+    var form = $('.apply-form');
+    if (!$('#apply-form__slide-2').isValid()) {
       return false;
     }
-    
-    showError("");
-    $(".spinner", applyForm).css('display', 'inline-block');
+
+    showError('');
+    $('.spinner', applyForm).css('display', 'inline-block');
 
     var formData = new FormData(form[0]);
 
@@ -147,20 +148,20 @@ $(document).ready(function() {
       success: function (data) {
         ga('send', 'event', 'Registration', 'registered');
 
-        $("#js-apply-form__email").text(data.email);
-        
-        $(".spinner", applyForm).css('display', 'none');
-        $(".apply-form__container").css('height', '');
+        $('#js-apply-form__email').text(data.email);
 
-        $(".slick-container").slick('slickNext');
+        $('.spinner', applyForm).css('display', 'none');
+        $('.apply-form__container').css('height', '');
+
+        $('.slick-container').slick('slickNext');
         $('html,body').animate({
           scrollTop: applyForm.offset().top
         }, 150, 'swing');
       },
       error: function(data) {
-        $(".spinner", applyForm).css('display', 'none');
+        $('.spinner', applyForm).css('display', 'none');
         data = data.responseJSON;
-        if(data.error) {
+        if (data.error) {
           showError(data.error);
         }
       },
@@ -175,20 +176,20 @@ $(document).ready(function() {
     ul.outerWidth(this.element.outerWidth());
   };
 
-  $("#instituton-radio").change(function() {
-    var uni = $("#institution-uni");
-    var hs = $("#institution-hs");
+  $('#instituton-radio').change(function() {
+    var uni = $('#institution-uni');
+    var hs = $('#institution-hs');
 
     var hide = null, show = null, label = null, majorRequired = false;
-    if($("#institution-radio-uni").is(':checked')) {
+    if ($('#institution-radio-uni').is(':checked')) {
       hide = hs;
       show = uni;
-      label = "University";
+      label = 'University';
       majorRequired = true;
     } else {
       show = hs;
       hide = uni;
-      label = "High School";
+      label = 'High School';
       majorRequired = false;
     }
 
@@ -200,32 +201,33 @@ $(document).ready(function() {
     hide.attr('name', '');
     hide.attr('data-validation', '');
 
-    if(majorRequired) {
-      $("#major").attr('required', true);
-      $("#major-label").addClass("apply-form__required");
+    if (majorRequired) {
+      $('#major').attr('required', true);
+      $('#major-label').addClass('apply-form__required');
     } else {
-      $("#major").attr('required', false);
-      $("#major-label").removeClass("apply-form__required");
+      $('#major').attr('required', false);
+      $('#major-label').removeClass('apply-form__required');
     }
 
-    $("#institution-label").text(label);
+    $('#institution-label').text(label);
   });
 
-  $("#outofstate").change(function() {
-    var outOfState = $("input[type=radio]:checked", this).val() === 'true';
-    if(outOfState) 
-      $("#city").attr('disabled', false);
-    else
-      $("#city").attr('disabled', true);
-  });
-
-  $("#js-apply-form__next").click(function() {
-    if($("#apply-form__slide-1").isValid()) {
-      $(".slick-container").slick('slickNext');
+  $('#outofstate').change(function() {
+    var outOfState = $('input[type=radio]:checked', this).val() === 'true';
+    if (outOfState) {
+      $('#city').attr('disabled', false);
+    } else {
+      $('#city').attr('disabled', true);
     }
   });
 
-  $("#js-apply-form__previous").click(function() {
-    $(".slick-container").slick('slickPrev');
+  $('#js-apply-form__next').click(function() {
+    if ($('#apply-form__slide-1').isValid()) {
+      $('.slick-container').slick('slickNext');
+    }
+  });
+
+  $('#js-apply-form__previous').click(function() {
+    $('.slick-container').slick('slickPrev');
   });
 });
