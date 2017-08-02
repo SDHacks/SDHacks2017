081319ada6813e6cb44c57dc6afc2a2e66a59e49
RedbackThomson <redbackthomson@gmail.com>
2017-08-02 22:11:52 +0800

    Fixed sponsor storybook

--- .storybook/stories/admin/resume.js
+++ .storybook/stories/admin/resume.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
-import {withKnobs, text, boolean, number, select, 
+import {withKnobs, text, boolean, number, select, object,
   array} from '@storybook/addon-knobs';
 import {withInfo} from '@storybook/addon-info';
 import {action} from '@storybook/addon-actions';
@@ -8,25 +8,6 @@ import {MemoryRouter} from 'react-router';
 
 import {Roles} from '~/static/Roles';
 import Sidebar from '~/components/apps/Admin/layouts/components/SponsorSidebar';
-
-let mockUsers = {
-  member: {
-    username: text('Username', 'Facetweet'),
-    role: Roles.ROLE_MEMBER
-  },
-  sponsor: {
-    username: text('Username', 'Facetweet'),
-    role: Roles.ROLE_SPONSOR
-  },
-  admin: {
-    username: text('Username', 'Facetweet'),
-    role: Roles.ROLE_ADMIN
-  },
-  developer: {
-    username: text('Username', 'Facetweet'),
-    role: Roles.ROLE_DEVELOPER
-  }
-};
 
 let roles = {};
 Object.values(Roles).forEach(role => roles[role] = role);
@@ -38,17 +19,34 @@ storiesOf('Resume Tool/Layout', module)
   ))
   .add('Overall', () => (
     <div className="admin-body d-flex flex-column">
-
       <div className="container-fluid p-0 w-100 h-100">
         <div className="d-flex flex-column flex-md-row h-100">
           <div className={`admin-sidebar__container 
             admin-sidebar__container--authenticated`}>
-            <Sidebar isEditing={boolean('isEditing', false)}
-              isAuthenticated
-              user={{
-                username: text('Username', 'Redback'),
-                role: select('Role', roles, Roles.ROLE_DEVELOPER)
-              }} onEditChange={action('Change edit')} />
+            <Sidebar user={{
+              username: text('Username', 'Facetweet'),
+              role: select('Role', roles, Roles.ROLE_SPONSOR)
+            }}
+            selected={number('Selected', 672)}
+            total={number('Total', 3367)}
+            toggleFilter={action('Toggle Filter')}
+            toggleFilterOption={action('Toggle Option')}
+            selectAllOptions={action('Select All')}
+            selectNoneOptions={action('Select None')}
+            filters={object('Filters', {
+              'github': {
+                displayName: 'Github',
+                enabled: true,
+                options: {
+                  'RedbackThomson': true
+                }
+              },
+              'year': {
+                displayName: 'Year',
+                enabled: false,
+                options: {}
+              }
+            })} />
           </div>
 
           <main style={{flex: 1}} className="p-3">
@@ -66,8 +64,29 @@ storiesOf('Resume Tool/Sidebar', module)
     <MemoryRouter initialEntries={['/admin/']}>{story()}</MemoryRouter>
   ))
   .add('Sponsor View', () => (
-    <Sidebar user={mockUsers.sponsor} selected={number('Selected', 672)}
-      total={number('Total', 3367)} toggleFilter={console.log}
-      toggleFilterOption={console.log} />
+    <Sidebar user={{
+        username: text('Username', 'Facetweet'),
+        role: select('Role', roles, Roles.ROLE_SPONSOR)
+      }}
+      selected={number('Selected', 672)}
+      total={number('Total', 3367)}
+      toggleFilter={action('Toggle Filter')}
+      toggleFilterOption={action('Toggle Option')}
+      selectAllOptions={action('Select All')}
+      selectNoneOptions={action('Select None')}
+      filters={object('Filters', {
+        'github': {
+          displayName: 'Github',
+          enabled: true,
+          options: {
+            'RedbackThomson': true
+          }
+        },
+        'year': {
+          displayName: 'Year',
+          enabled: false,
+          options: {}
+        }
+      })} />
   ))
 ;
