60472c76bf2ff761bec1560af0834ab82e9f7f35
liawesomesaucer <vincent.oddrepublic@gmail.com>
2017-08-20 17:39:06 -0700

    Minor UI changes

--- static/app/components/apps/Apply/pages/ApplyPage/components/SubmittedSection.js
+++ static/app/components/apps/Apply/pages/ApplyPage/components/SubmittedSection.js
@@ -2,14 +2,17 @@ import React from 'react';
 
 class SubmittedSection extends React.Component {
   render() {
-    return (<div className="container">
-      <div className="row">
-        <div className="col-sm-12 text-center">
-          <h3>You have successfully applied for SD Hacks 2017!</h3>
-          <h5>Please check your email for confirmation</h5>
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12 text-center">
+            <h3>You have successfully applied for SD Hacks 2017!</h3>
+            <h5>Please check your email for confirmation</h5>
+            <a href="/" className="rounded-button">Home</a>
+          </div>
         </div>
       </div>
-    </div>);
+    );
   }
 };
 
