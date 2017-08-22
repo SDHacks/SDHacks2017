51356744c44a1b49e0de951bdc24c6834766a877
RedbackThomson <redbackthomson@gmail.com>
2017-08-22 16:12:15 -0700

    Fixed missing major

--- entities/users/model.js
+++ entities/users/model.js
@@ -12,6 +12,7 @@ var Schema = mongoose.Schema;
 var db = mongoose.createConnection(process.env.MONGODB_URI);
 
 var UserSchema = new Schema({
+  // Declares the user's login username
   username: {
     type: String,
     trim: true,
@@ -19,29 +20,35 @@ var UserSchema = new Schema({
     lowercase: true,
     required: [true, 'You must have a username']
   },
+  // Declares the user's login password
   password: {
     type: String,
     trim: true,
     required: [true, 'You must have a password']
   },
+  // Declares the user's first name
   firstName: {
     type: String,
     trim: true,
     required: [true, 'You must have a first name']
   },
+  // Declares the user's last name
   lastName: {
     type: String,
     trim: true,
     required: [true, 'You must have a last name']
   },
+  // Declares the user's birth date
   birthdate: {
     type: Date,
     required: [true, 'You must have a birthdate']
   },
+  // Declares the user's gender
   gender: {
     type: String,
     required: [true, 'You must have a gender']
   },
+  // Declares the user's email address
   email: {
     type: String,
     required: [true, 'You must have an email'],
@@ -51,77 +58,110 @@ var UserSchema = new Schema({
     match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
       'You must use a valid email']
   },
+  // Declares the user's phone number
   phone: {
     type: Number,
     required: [true, 'You must have a phone number']
   },
+  // Declares which university the user attends
   university: {
     type: String,
     trim: true,
     required: [true, 'You must have a university']
   },
-  majors: [{type: String, trim: true}],
-  categories: [{type: String, trim: true}],
+  // Declares which major the user has specified
+  major: {
+    type: String,
+    trim: true
+  },
+  // Declares which category the user fits under
+  categories: [{
+    type: String,
+    trim: true
+  }],
+  // Declares which year the student is currently attending
   year: {
     type: String,
     required: [true, 'You must give a year in school']
   },
+  // Declares the user's Github account name
   github: {
     type: String,
     trim: true,
     required: false,
   },
+  // Declares the user's personal website link
   website: {
     type: String,
     trim: true,
     required: false,
   },
+  // Declares whether the user has given permission for their resume to be
+  // shared
   shareResume: {
     type: Boolean,
     default: false
   },
+  // Declares the food that the user has requested
   food: {
     type: String,
     trim: true,
   },
+  // Declares what dietary requirements the user has
   diet: {
     type: String,
     trim: true,
   },
+  // Declares the size of the shirt that the user has requested
   shirtSize: {
     type: String,
     required: [true, 'You must have a shirt size']
   },
   travel: {
+    // Declares whether the user will be travelling from outside of the state
     outOfState: {
       type: Boolean,
       default: false
     },
+    // Declares which city the user will be travelling from
     city: {
       type: String
     }
   },
+  // Declares the name of the bus that the user can take
   availableBus: {
     type: String,
     trim: true
   },
+  // Declares whether the user has noted they will be taking the bus
   bussing: {
     type: Boolean,
     default: false
   },
-  teammates: [{type: String, match:
-  [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-    'You must use a valid email']}],
+  // Declares the array of teammates that the user has defined
+  teammates: [{
+    type: String,
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'You must use a valid email']
+  }],
+  // Declares the user has confirmed their email address
   confirmed: {
     type: Boolean,
     default: false
   },
-  //Rejected, Unconfirmed, Confirmed, Declined, Late, and Waitlisted
+  // Declares the user's current application status
+  // Rejected, Unconfirmed, Confirmed, Declined, Late, and Waitlisted
   status: {
     type: String,
     trim: true
   },
+  // Declares that the user has checked into the event on the day
   checkedIn: {
+    type: Boolean,
+    default: false
+  },
+  // Declares that the user's information is ready for sponsor consumption
+  sanitized: {
     type: Boolean,
     default: false
   }
