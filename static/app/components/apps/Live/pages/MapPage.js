b303a0fb1f7cfce860fa4a90cbcd5692a1720b9b
Redback <thomsonnicholas@hotmail.com>
2017-10-20 21:38:33 -0700

    Embedded Map from S3

--- static/app/components/apps/Live/pages/MapPage.js
+++ static/app/components/apps/Live/pages/MapPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MapPage = () => {
   return (
     <div className="map-page bg-white h-100">
-      <img className="mw-100 mh-100" src="assets/img/map.svg" />
+      <img className="mw-100 mh-100" src="https://s3-us-west-1.amazonaws.com/sdhacks2017-production/assets/map.svg" />
     </div>
   );
 };
