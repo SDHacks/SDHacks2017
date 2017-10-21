a1937403e516e67ee36c02da5cd94d2d762d5dc5
liawesomesaucer <vincent.oddrepublic@gmail.com>
2017-10-20 20:52:53 -0700

    Update schedule with locations

--- static/app/components/apps/Live/pages/SchedulePage.js
+++ static/app/components/apps/Live/pages/SchedulePage.js
@@ -12,7 +12,7 @@ export default class SchedulePage extends React.Component {
         {/* <p>This is the Schedule Page!</p> */}
         <div className="container">
           <div className="row">
-            <div className="col-lg-4">
+            <div className="col-lg-12">
               <div className="sd-card__wrapper">
                 <div className="card sd-card sd-card__schedule">
                   <div className="card-body sd-card__block">
@@ -23,46 +23,56 @@ export default class SchedulePage extends React.Component {
                     </div>
                     <ul className="sd-schedule__list list-unstyled">
                       <li className="sd-schedule__list-elem">
+                        <span className="sd-schedule__time">7pm</span>
                         <span className="sd-schedule__event">Check In</span>
-                        <span className="sd-schedule__time">7pm</span>
+                        <span className="sd-schedule__location">&nbsp;</span>
                       </li>
                       <li className="sd-schedule__list-elem">
+                        <span className="sd-schedule__time">7pm</span>
                         <span className="sd-schedule__event">Dinner</span>
-                        <span className="sd-schedule__time">7pm</span>
+                        <span className="sd-schedule__location">Lobby</span>
                       </li>
                       <li className="sd-schedule__list-elem">
-                        <span className="sd-schedule__event">Opening Ceremony</span>
                         <span className="sd-schedule__time">9pm</span>
+                        <span className="sd-schedule__event">Opening Ceremony</span>
+                        <span className="sd-schedule__location">Main Stage</span>
                       </li>
                       <li className="sd-schedule__list-elem">
-                        <span className="sd-schedule__event">Hacking Begins</span>
                         <span className="sd-schedule__time">10pm</span>
+                        <span className="sd-schedule__event">Hacking Begins</span>
+                        <span className="sd-schedule__location">&nbsp;</span>
                       </li>
                       <li className="sd-schedule__list-elem">
-                        <span className="sd-schedule__event">Team Mixer</span>
                         <span className="sd-schedule__time">10-10:30pm</span>
+                        <span className="sd-schedule__event">Team Mixer</span>
+                        <span className="sd-schedule__location">2nd floor</span>
                       </li>
                       <li className="sd-schedule__list-elem">
+                        <span className="sd-schedule__time">10:30-11:15pm</span>
                         <span className="sd-schedule__event">Marines Innovation Challenge Info Session</span>
-                        <span className="sd-schedule__time">10:30-11:15pm</span>
+                        <span className="sd-schedule__location">Green Room</span>
                       </li>
                       <li className="sd-schedule__list-elem">
+                        <span className="sd-schedule__time">10:30-11:15pm</span>
                         <span className="sd-schedule__event">Qualcomm DragonBoard Workshop</span>
-                        <span className="sd-schedule__time">10:30-11:15pm</span>
-                      </li><li className="sd-schedule__list-elem">
-                        <span className="sd-schedule__event">Virtual Reality and Gaming Workshop</span>
-                        <span className="sd-schedule__time">11:30pm-2:30am</span>
+                        <span className="sd-schedule__location">Dugout Room</span>
                       </li>
                       <li className="sd-schedule__list-elem">
-                        <span className="sd-schedule__event">Nodeschool Workshop</span>
+                        <span className="sd-schedule__time">11:30pm-2:30am</span>
+                        <span className="sd-schedule__event">Virtual Reality and Gaming Workshop</span>
+                        <span className="sd-schedule__location">Green Room</span>
+                      </li>
+                      <li className="sd-schedule__list-elem">
                         <span className="sd-schedule__time">11:30pm-1:30am</span>
+                        <span className="sd-schedule__event">Nodeschool Workshop</span>
+                        <span className="sd-schedule__location">Dugout Room</span>
                       </li>
                     </ul>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="col-lg-4">
+            <div className="col-lg-12">
               <div className="sd-card__wrapper">
                 <div className="card sd-card sd-card__schedule">
                   <div className="card-body sd-card__block">
@@ -71,35 +81,41 @@ export default class SchedulePage extends React.Component {
                     </div>
                     <ul className="sd-schedule__list list-unstyled">
                       <li className="sd-schedule__list-elem">
-                        <span className="sd-schedule__event">iOS Workshop</span>
                         <span className="sd-schedule__time">2-4am</span>
+                        <span className="sd-schedule__event">iOS Workshop</span>
+                        <span className="sd-schedule__location">Dugout Room</span>
                       </li>
                       <li className="sd-schedule__list-elem">
-                        <span className="sd-schedule__event">Breakfast</span>
                         <span className="sd-schedule__time">8:30am</span>
+                        <span className="sd-schedule__event">Breakfast</span>
+                        <span className="sd-schedule__location">Lobby</span>
                       </li>
                       <li className="sd-schedule__list-elem">
-                        <span className="sd-schedule__event">SPAWAR Tech Talk</span>
                         <span className="sd-schedule__time">11am-1pm</span>
+                        <span className="sd-schedule__event">SPAWAR Tech Talk</span>
+                        <span className="sd-schedule__location">Green Room</span>
                       </li>
                       <li className="sd-schedule__list-elem">
-                        <span className="sd-schedule__event">Lunch</span>
                         <span className="sd-schedule__time">1pm</span>
+                        <span className="sd-schedule__event">Lunch</span>
+                        <span className="sd-schedule__location">Lobby</span>
                       </li>
                       <li className="sd-schedule__list-elem">
-                        <span className="sd-schedule__event">Getting Into Game Development</span>
                         <span className="sd-schedule__time">3-3:45pm</span>
+                        <span className="sd-schedule__event">Getting Into Game Development</span>
+                        <span className="sd-schedule__location">Green Room</span>
                       </li>
                       <li className="sd-schedule__list-elem">
-                        <span className="sd-schedule__event">Dinner</span>
                         <span className="sd-schedule__time">7:30pm</span>
+                        <span className="sd-schedule__event">Dinner</span>
+                        <span className="sd-schedule__location">Lobby</span>
                       </li>
                     </ul>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="col-lg-4">
+            <div className="col-lg-12">
               <div className="sd-card__wrapper">
                 <div className="card sd-card sd-card__schedule">
                   <div className="card-body sd-card__block">
@@ -108,24 +124,29 @@ export default class SchedulePage extends React.Component {
                     </div>
                     <ul className="sd-schedule__list list-unstyled">
                       <li className="sd-schedule__list-elem">
-                        <span className="sd-schedule__event">Breakfast</span>
                         <span className="sd-schedule__time">7am</span>
+                        <span className="sd-schedule__event">Breakfast</span>
+                        <span className="sd-schedule__location">Lobby</span>
                       </li>
                       <li className="sd-schedule__list-elem">
+                        <span className="sd-schedule__time">10am</span>
                         <span className="sd-schedule__event">Lunch</span>
-                        <span className="sd-schedule__time">10am</span>
+                        <span className="sd-schedule__location">Lobby</span>
                       </li>
                       <li className="sd-schedule__list-elem">
+                        <span className="sd-schedule__time">10am</span>
                         <span className="sd-schedule__event">Hacking Ends</span>
-                        <span className="sd-schedule__time">10am</span>
+                        <span className="sd-schedule__location"></span>
                       </li>
                       <li className="sd-schedule__list-elem">
-                        <span className="sd-schedule__event">Demos and Judging</span>
                         <span className="sd-schedule__time">11am-1pm</span>
+                        <span className="sd-schedule__event">Demos and Judging</span>
+                        <span className="sd-schedule__location">Main Stage</span>
                       </li>
                       <li className="sd-schedule__list-elem">
-                        <span className="sd-schedule__event">Closing Ceremony</span>
                         <span className="sd-schedule__time">1-2pm</span>
+                        <span className="sd-schedule__event">Closing Ceremony</span>
+                        <span className="sd-schedule__location">Main Stage</span>
                       </li>
                     </ul>
                   </div>
